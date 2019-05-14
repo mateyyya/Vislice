8c0ca20742ba237bc4bc3a54d4d898aef1659bd3
mateyyya <mateja.cuk5@gmail.com>
2019-05-14 15:33:40 +0200

    funkcije stevilo napak, zmaga in poraz

---
+++ model.py
@@ -0,0 +1,37 @@
+STEVILO_DOVOLJENIH_NAPAK = 10
+PRAVILNA_CRKA = '+'
+PONOVLJENA_CRKA = 'o'
+NAPACNA_CRKA = '-'
+
+ZMAGA = 'W'
+PORAZ = 'X'
+
+class Igra:
+    def __init__(self, geslo):
+        self.geslo = geslo
+        self.crke = []
+
+    def napacne_crke(self):
+        return [c for c in self.crke if c not in self.geslo]
+
+    def pravilne_crke(self):
+        return [c for c in self.crke if c in self.geslo]
+
+    def stevilo_napak(self):
+        return len(self.napacne_crke())
+
+    def zmaga(self):
+        return all(c in self.crke for c in self.geslo)
+    
+    def poraz(self):
+        return self.stevilo_napak() >= STEVILO_DOVOLJENIH_NAPAK
+
+
+
+igra = Igra("nekaj")
+igra.crke = ['a', 'l', 'v', 'n', 'x', 'e', 'k', 'j']
+print(igra.napacne_crke())
+print(igra.pravilne_crke())
+print(igra.stevilo_napak())
+print(igra.zmaga())
+print(igra.poraz())
