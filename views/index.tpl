533fdba3c90d0333e4007b02c83ce3ad10dfdb6c
mateyyya <mateja.cuk5@gmail.com>
2019-05-21 17:09:16 +0200

    vislice spletna verzija

--- views/index.tpl
+++ views/index.tpl
@@ -12,7 +12,7 @@
 
   <img src="img/10.jpg" alt="obesanje">
 
-  <form action="/igra/" method="post">
+  <form action="/igra/" method="get">
     <button type="submit">Nova igra</button>
   </form>
 </body>
--- vislice.py
+++ vislice.py
@@ -4,8 +4,7 @@ import model
 import bottle
 
 vislice = model.Vislice()
-id = vislice.nova_igra()
-igra, poskus = vislice.igre[id]
+# id = vislice.nova_igra()
 
 @bottle.get('/')
 def indeks():
@@ -16,8 +15,20 @@ def vrni_slike(ime):
     return bottle.static_file(ime, root="img")
 
 @bottle.get('/igra/')
-def igratest():
+def nova_igra():
+    id = vislice.nova_igra()
+    bottle.redirect('/igra/{0}/'.format(id))
+
+@bottle.get('/igra/<id:int>/')
+def pokazi_igro(id):
+    igra, poskus = vislice.igre[id]
     return bottle.template('igra.html', id_igre=id, igra=igra, poskus=poskus)
+
+@bottle.post('/igra/<id:int>/')
+def ugibaj(id):
+    crka = bottle.request.forms.getunicode('crka')
+    vislice.ugibaj(id, crka)
+    bottle.redirect('/igra/{0}/'.format(id))
 
 
 bottle.run(reloader=True, debug=True)
